26f36bc5e77a2e2c34e4366e29457aa2e0777f8c
james.zhang <32316075+trzhang-ai@users.noreply.github.com>
2026-08-08 02:17:22 +0800

    fix: enforce NYC geographic boundaries

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -26,9 +26,15 @@ def go(args):
     ######################
     artifact_local_path = run.use_artifact(args.input_artifact).file()
     df = pd.read_csv(artifact_local_path)
-    idx = df['price'].between(args.min_price, args.max_price)
-    df = df[idx].copy()
+    df = df.drop_duplicates()
+    df = df.dropna(subset=["price"])
+    df = df[df["price"].between(args.min_price, args.max_price)]
     df['last_review'] = pd.to_datetime(df['last_review'])
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+    logger.info("Cleaned data has %s rows and %s columns", *df.shape)
+
+    # Save cleaned data
     df.to_csv("clean_sample.csv", index=False)
     artifact = wandb.Artifact(
         args.output_artifact,
